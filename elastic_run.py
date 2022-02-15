1963680512926ee028ddf856a9dae5622385d3fa
Dmitri Voytan <dvoytan@utexas.edu>
2022-02-15 00:03:48 -0600

    First working test

--- elastic_run.py
+++ elastic_run.py
@@ -3,8 +3,9 @@ import tqdm
 import matplotlib.pyplot as plt
 import os
 import yaml
-from datetime import date
+from datetime import datetime, date
 from argparse import ArgumentParser
+from scipy.interpolate import interp1d
 
 from devito import norm
 from devito.logger import info
@@ -14,13 +15,12 @@ from examples.seismic.elastic import ElasticWaveSolver
 from examples.seismic import demo_model, setup_geometry, seismic_args, AcquisitionGeometry
 from examples.seismic.model import SeismicModel
 
-
 def initialize_output(save_path):
     today = date.today()
     time = datetime.now()
     outdir = os.path.join(save_path, today.strftime("%b-%d-%Y"), time.strftime("%H:%M:%S"))
     os.makedirs(outdir)
-    return save_outdir
+    return outdir
 
 def parse_config_to_kwargs(config):
     kwargs = {}
@@ -29,10 +29,10 @@ def parse_config_to_kwargs(config):
             kwargs[val] = config[item][val]
     return kwargs
 
-def resample_gather(config, data):
-    new_dt = config['time_axis']['resample_dt']
-    t_final = config['time_axis']['tn']
-    assert type(new_dt) in {'float', 'float32', 'float64'}
+def resample_gather(config, geometry, data):
+    new_dt = config['solver']['resample_dt']
+    t_final = config['solver']['tn']
+    assert type(new_dt) == float
 
     #For aco, we resample to the tti sampling rate
     new_nt = int(t_final/new_dt)
@@ -43,48 +43,51 @@ def resample_gather(config, data):
 
     for i in range(data.shape[-1]):
         interp = interp1d(geometry.time_axis.time_values, data[:,i])
-        tmp = interp(time_axis)
+        tmp = interp(new_time_axis)
         resample_data[:,i] = tmp
 
     return resample_data
 
-def get_bottom_padding(model, config):
-    dx = model.spacing[0]
-    dz = model.spacing[-1]
-
-    index = config['model']['src_x']/dx
-    slc = model.vp[index, :]
+def get_bottom_padding(vp, config):
+    model_config = config['model']
+    dx = model_config['spacing'][0]
+    dz = model_config['spacing'][1]
+    index = int(config['acquisition']['src_x']/dx)
+    slc = vp[index, :]
 
     #Get the two-way traveltime for a vertical ray
-    t = 2*np.sum(slc*dz)*1000 
+    t = 2*np.sum(slc*(dz/1000)) #seconds
     tn = config['solver']['tn']
 
-    if t >= tn:
-        bottom_pad = (tn-n)*slc[-1]/(2*dx)
-        bottom_pad = int(np.round(bottom_pad)) + 1 
+    if t <= tn:
+        bottom_pad = (1./2.)*(tn-t)*slc[-1]/(dx/1000)
+        bottom_pad = int(np.round(bottom_pad)) + 1
     else:
         bottom_pad = 0
     return bottom_pad
     
-def pad_based_on_source(src_x, spacing_x, model, config):
+def pad_based_on_source(vp, config):
     '''Pad either the left or right side of the model so that the source is centered'''
+
+    src_x = config['acquisition']['src_x']
+    dx = config['model']['spacing'][0]
     pad_dict = {}
-    pad_dict['bottom'] = get_bottom_padding(model, config)
+    pad_dict['bottom'] = get_bottom_padding(vp, config)
     pad_dict['top'] = 0
-    model_width = model.spacing[0]*model.shape[0]
+    model_width = dx*config['model']['shape'][0]
     half_model_width = model_width/2
     if src_x > half_model_width: #half the model width
-        pad_dist = int((src_x - half_model_width)/spacing_x)
+        pad_dist = int((src_x - half_model_width)/dx)
         pad_dict['right'] = pad_dist
         pad_dict['left'] = 0
     else:
-        pad_dist = int((half_model_width-src_x)/spacing_x)
+        pad_dist = int((half_model_width-src_x)/dx)
         pad_dict['left'] = pad_dist
         pad_dict['right'] = 0
     return pad_dict
 
 
-def setup_model(config):
+def setup_model(config, **kwargs):
 
     model_config = config['model']
 
@@ -99,15 +102,19 @@ def setup_model(config):
     space_order = config['solver']['space_order']
     dtype = np.float32 if config['solver']['dtype']== 'float32' else np.float64
     
-    vp = np.load(vp_path).astype(dtype)
-    vs = np.load(vs_path).astype(dtype)
-    rho = np.load(rho_path).astype(dtype)
+    #Load data and convert to km/s and kg/km^3
+    vp = np.load(vp_path).astype(dtype)/1000.0
+    vs = np.load(vs_path).astype(dtype)/1000.0
+    rho = np.load(rho_path).astype(dtype)/1000.0
 
-    padding = pad_based_on_source(src_coord[0], spacing[0], model, config)
+    padding = pad_based_on_source(vp, config)
 
+    params = [vp, vs, rho]
     #Pad the params
-    vp = np.pad(vp, pad_width=((padding['left'],padding['right']),\
+    for i, param in enumerate(params):
+        params[i] = np.pad(params[i], pad_width=((padding['left'],padding['right']),\
                                                 (padding['top'],padding['bottom'])), mode='edge')
+    vp, vs, rho = params
 
     #Correct origin based on npad if padding left (negative value)
     if padding['left'] > 0:
@@ -117,15 +124,15 @@ def setup_model(config):
 
     origin = tuple([x_origin, 0.])
     
-    return SeismicModel(space_order=space_order, vp=vp,
+    return SeismicModel(space_order=space_order, vp=vp, vs=vs, b=1/rho,
                             origin=origin, shape=shape, dtype=dtype, spacing=spacing,
                             nbl=nbl, **kwargs)
 
 def setup_geometry(model, config):
 
     acq_config = config['acquisition']
-    tn = config['solver']['tn']
-    f0 = config['solver']['f0']
+    tn = config['solver']['tn']*1000
+    f0 = config['source']['f0']/1000
     src_type = 'Ricker'
     src_coord = (acq_config['src_x'], acq_config['src_z'])
     rcv_coords = (acq_config['rec_x'], acq_config['rec_z'])
@@ -147,12 +154,12 @@ def setup_rec_coords(model, rcv_coords):
     recx = rcv_coords[0]
     recx = np.arange(recx[0], recx[1], recx[2])*model.spacing[0]
 
-    rec_coordinates = np.empty((recv_x, model.dim))
+    rec_coordinates = np.empty((len(recx), model.dim))
     rec_coordinates[:, 0] = recx
     rec_coordinates[:, -1] = rcv_coords[-1]
     return rec_coordinates
 
-def elastic_setup(config, **kwargs):
+def elastic_setup(config):
 
     model = setup_model(config)
     critical_dt = model.critical_dt
@@ -160,15 +167,16 @@ def elastic_setup(config, **kwargs):
     # Source and receiver geometries
     geometry = setup_geometry(model, config)
 
+    #solver
+    space_order = config['solver']['space_order']
+
     # Create solver object to provide relevant operators
-    solver = ElasticWaveSolver(model, geometry, space_order=space_order, **kwargs)
+    solver = ElasticWaveSolver(model, geometry, space_order=space_order)
     return solver, critical_dt, geometry
 
 def run(config):
 
-    kwargs = parse_config_to_kwargs(config)
-
-    solver, critical_dt, geometry = elastic_setup(config, **kwargs)
+    solver, critical_dt, geometry = elastic_setup(config)
     info("Applying Forward")
 
     rec1, rec2, v, tau, summary = solver.forward(autotune=config['solver']['autotune'])
@@ -188,17 +196,23 @@ if __name__ == '__main__':
     #Create output directory structure
     outdir=initialize_output(config['output']['outdir'])
 
+    #save the config
+    with open(os.path.join(outdir, "config.yaml"), "w") as output:
+        yaml.dump(config, output, default_flow_style=False)
+
     #Run the solver
     rec1, rec2, v, tau, summary, geometry = run(config)
 
     #Resample gather to requested dt
-    if config['time_axis']['resample_dt'] is not None:
-        data = resample_gather(data, config)
+    if config['solver']['resample_dt'] is not None:
+        rec1 = resample_gather(config, geometry, rec1.data)
+        rec2 = resample_gather(config, geometry, rec2.data)
 
     #save
-    save_file = {'shot_gather':data,
-                'src':geometry.src_positions,
-                'rcv':geometry.rcv_positions}
+    save_file = {'v_1':rec1,
+                 'v_2':rec2,
+                 'src':geometry.src_positions,
+                 'rcv':geometry.rec_positions}
 
     f_name = config['output']['file_prefix']
     np.save(os.path.join(outdir, f_name), save_file)
