1f892fbe6c7f80f98347c467786ef787bedfe031
Dmitri Voytan <dvoytan@utexas.edu>
2022-03-07 11:17:49 -0600

    corrected padding traveltime calculations

--- utils/pad_utils.py
+++ utils/pad_utils.py
@@ -9,13 +9,13 @@ def get_bottom_padding(vp, config, src_x, src_z):
     slc = vp[src_x, src_z:]
 
     # Get the two-way traveltime for a vertical ray
-    tz = 2*np.sum(slc*(dz))  # seconds
+    tz = 2*np.sum(dz*(1/slc))  # seconds
     tn = config['solver']['tn']
 
     # Get the padding if necessary
     if tz <= tn:
         # dz is converted from from m to km
-        bottom_pad = (1./2.)*(tn-tz)*slc[-1]/(dz)
+        bottom_pad = (0.50)*(tn-tz)*(slc[-1])/(dz)
         bottom_pad = int(np.round(bottom_pad)) + 1
     else:
         bottom_pad = 0
@@ -24,7 +24,7 @@ def get_bottom_padding(vp, config, src_x, src_z):
 
 def get_side_padding(vp, config, src_x, src_z):
     '''
-    Designed to remove the direct wave reflection propogating at 
+    Designed to remove the direct wave reflection propagating at 
     the surface (typically water) velocity, but may not remove edge 
     reflections e.g. if there is a fast medium in the shallow subsurface 
     such as a large salt block.
@@ -37,21 +37,21 @@ def get_side_padding(vp, config, src_x, src_z):
     tn = config['solver']['tn']
 
     # Get the traveltime to the right and left side of the model.
-    right_tt = np.sum(slc_right*(dx))
-    left_tt = np.sum(slc_left*(dx))
+    right_tt = np.sum((1/slc_right)*(dx))
+    left_tt = np.sum((1/slc_left)*(dx))
 
     # Get padding amounts
-    # For the direct wave we only need 1/2 the residual travel time in 
-    # padding. Using 0.60 gives a bit more room for error
+    # For the direct wave we only need 1/2 the residual travel time in
+    # padding. Using 0.65 gives a bit more room for error
     if right_tt <= tn:
         pad_right = (0.65)*(tn-right_tt)*slc_right[-1]/(dx)
-        pad_right = int(np.round(pad_right)) + 2  # add a few extra points
+        pad_right = int(np.round(pad_right)) + 1  # add an extra point for roundoff error
     else:
         pad_right = 0
 
     if left_tt <= tn:
         pad_left = (0.65)*(tn-left_tt)*slc_left[-1]/(dx)
-        pad_left = int(np.round(pad_left)) + 2  # add a few extra points
+        pad_left = int(np.round(pad_left)) + 1  # add an extra point for roundoff error
     else:
         pad_left = 0
 
